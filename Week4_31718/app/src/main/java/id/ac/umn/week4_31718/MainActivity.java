e9fd7856c14a01fc8dca4d0e9e1f5299a775cf79
Hnsenchristiann <christiansensen00@gmail.com>
2021-02-18 20:30:21 +0700

    Week04 (Intent)

---
+++ Week4_31718/app/src/main/java/id/ac/umn/week4_31718/MainActivity.java
@@ -0,0 +1,74 @@
+package id.ac.umn.week4_31718;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText etIsian, etURL;
+    private Button btnKirim, btnBrowse;
+    private TextView tvJawaban;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        etIsian = findViewById(R.id.isian);
+        etURL = findViewById(R.id.url);
+        btnBrowse = findViewById(R.id.buttonBrowse);
+        btnKirim = findViewById(R.id.buttonKirim);
+        tvJawaban = findViewById(R.id.jawaban);
+
+
+        btnKirim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentDua = new Intent(MainActivity.this,
+                                       ActivityDua.class);
+                String isian = etIsian.getText().toString();
+                intentDua.putExtra("PesanDariMain", isian);
+                startActivityForResult(intentDua, 1);
+            }
+
+        });
+
+
+
+
+        btnBrowse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String urlText = etURL.getText().toString();
+                if(urlText.isEmpty()){
+                    urlText = "https://www.umn.ac.id/";
+                }
+                Intent browseIntent = new Intent(Intent.ACTION_VIEW);
+                browseIntent.setData(Uri.parse(urlText));
+                if(browseIntent.resolveActivity(getPackageManager()) != null){
+                    startActivity(browseIntent);
+                }
+            }
+
+        });
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                String jawaban = data.getStringExtra("Jawaban");
+                tvJawaban.setText(jawaban);
+            }
+        }
+    }
+
+}
